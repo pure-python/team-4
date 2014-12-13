1e0873bb057956639e343319cdbbc907999d0e8b
Gabriela Busca <buscagabriela@yahoo.com>
2014-12-13 17:09:23 +0200

    edit posts

--- purepython/urls.py
+++ purepython/urls.py
@@ -5,7 +5,8 @@ from django.conf.urls.static import static
 
 from fb.views import (
     index, post_details, login_view, logout_view, profile_view,
-    edit_profile_view, like_view, user_album, user_album_photos
+    edit_profile_view, like_view, user_album, user_album_photos,
+    edit_post_view
 )
 
 
@@ -16,6 +17,7 @@ urlpatterns = patterns(
     url(r'^$', index, name='index'),
     url(r'^post/(?P<pk>\d)/$', post_details, name='post_details'),
     url(r'^post/(?P<pk>\d)/like$', like_view, name='like'),
+    url(r'^post/(?P<pk>\d)/edit$', edit_post_view, name='edit_post'),
     url(r'^accounts/login/$', login_view, name='login'),
     url(r'^accounts/logout/$', logout_view, name='logout'),
     url(r'^profile/(?P<user>\w+)/$', profile_view, name='profile'),
